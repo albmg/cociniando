130db4487c782814a2d50eb65e9416629f4a0b32
albmg <albmguer@gmail.com>
2021-02-22 01:27:39 +0000

    feat(top tab bar): add top tab bar to create recipe

---
+++ app/components/take-photo/take-photo.story.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { storiesOf } from "@storybook/react-native"
+import { StoryScreen, Story, UseCase } from "../../../storybook/views"
+import { color } from "../../theme"
+import { TakePhoto } from "./take-photo"
+
+storiesOf("TakePhoto", module)
+  .addDecorator(fn => <StoryScreen>{fn()}</StoryScreen>)
+  .add("Style Presets", () => (
+    <Story>
+      <UseCase text="Primary" usage="The primary.">
+        <TakePhoto style={{ backgroundColor: color.error }} />
+      </UseCase>
+    </Story>
+  ))
--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -3,16 +3,21 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
 import { RecipesScreen } from "../screens/recipes/recipes-screen"
 import { SingleRecipeScreen } from "../screens/singleRecipe/singleRecipe-screen"
 import { CreateRecipeScreen } from "../screens/createRecipe/createRecipe-screen"
+import { DescriptionRecipeScreen } from "../screens/description-recipe/description-recipe-screen"
+import { TakePhotoScreen } from "../screens/take-photo/take-photo-screen"
+import { TakePhoto } from "../components/take-photo/take-photo"
 
 // import Ionicons from '@expo/vector-icons/Ionicons'
 import { State } from 'react-native-gesture-handler'
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
+const TopTab = createMaterialTopTabNavigator()
 
 console.log(State)
 
@@ -20,14 +25,22 @@ export const StackNavigator = () => (
   <Stack.Navigator>
     <Stack.Screen name="Recipes" component={RecipesScreen} />
     <Stack.Screen name="SingleRecipe" component={SingleRecipeScreen} />
+    <Stack.Screen name="TakePhoto" component={TakePhoto} />
   </Stack.Navigator>
+)
+
+export const TopTabNavigator = () => (
+  <TopTab.Navigator>
+    <Tab.Screen name="Descripción" component={DescriptionRecipeScreen} />
+    <Tab.Screen name="Crear" component={CreateRecipeScreen} />
+  </TopTab.Navigator>
 )
 
 export const TabNavigator = () => (
   <NavigationContainer>
     <Tab.Navigator>
       <Tab.Screen name="Home" component={StackNavigator} />
-      <Tab.Screen name="Crear" component={CreateRecipeScreen} />
+      <Tab.Screen name="Crear" component={TopTabNavigator} />
     </Tab.Navigator>
   </NavigationContainer>
 )
